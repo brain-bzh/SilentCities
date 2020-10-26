7a726ef61d83ad3998efa75de2d1040126f3437f
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-10-26 23:55:48 +0100

    script to move sites from one disk to the other

---
+++ utils/move_duplicates.py
@@ -0,0 +1,26 @@
+import os,sys,shutil
+firstdisk = sys.argv[1] 
+seconddisk = sys.argv[2]
+
+folderlist = os.listdir(firstdisk)
+secondlist = os.listdir(seconddisk)
+
+for folder in folderlist:
+
+    if folder in secondlist:
+        print("Folder {} was found on the second disk".format(folder))
+        print("Moving all subfolders and files of {} to {}".format(os.path.join(firstdisk,folder),os.path.join(seconddisk,folder)))
+        listsub = os.listdir(os.path.join(firstdisk,folder))
+        for subfold in listsub:
+
+            if os.path.isdir(os.path.join(firstdisk,folder,subfold)):
+                print("moving subfolder {}".format(os.path.join(firstdisk,folder,subfold)))
+                #shutil.copytree(os.path.join(firstdisk,folder,subfold),os.path.join(seconddisk,folder,subfold))
+                shutil.move(os.path.join(firstdisk,folder,subfold),os.path.join(seconddisk,folder))
+                #print("removing subfolder {}".format(os.path.join(firstdisk,folder,subfold)))
+                #shutil.rmtree(os.path.join(firstdisk,folder,subfold))
+            else:
+                print("moving file {}".format(os.path.join(firstdisk,folder,subfold)))
+                shutil.move(os.path.join(firstdisk,folder,subfold),os.path.join(seconddisk,folder))
+        print("removing folder {}".format(os.path.join(firstdisk,folder)))
+        shutil.rmtree(os.path.join(firstdisk,folder))
