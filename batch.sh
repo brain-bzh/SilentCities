4e572dc75afaf985d6b898259b9fc464015f33aa
samuel <samuel@samuel.com>
2021-07-09 10:39:39 +0200

    only tagging + faute de frappe

--- batch.sh
+++ batch.sh
@@ -1,9 +1,9 @@
-for CURSITE in $(ls /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/)
-do
-	echo python Ecoacoustic_test.py --site $CURSITE --data_path /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/${CURSITE}/ --save_path /Volumes/LaCie/Dataset_OHM_PYRENEES/2020-results/
-done
+#for CURSITE in $(ls /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/)
+#do
+#	python Ecoacoustic_test.py --site $CURSITE --data_path /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/${CURSITE}/ --save_path /Volumes/LaCie/Dataset_OHM_PYRENEES/2020-results/
+#done
 
 for CURSITE in $(ls /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/)
 do
-	echo python taggin_ohm.py --site $CURSITE --data_path /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/${CURSITE}/ --save_path /Volumes/LaCie/Dataset_OHM_PYRENEES/2020-results/
+	python tagging_ohm.py --site $CURSITE --data_path /Volumes/LaCie/Dataset_OHM_PYRENEES/OHM_PYRENEES_2020/${CURSITE}/ --save_path /Volumes/LaCie/Dataset_OHM_PYRENEES/2020-results/
 done
