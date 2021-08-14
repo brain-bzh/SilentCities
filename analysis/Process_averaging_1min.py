154632afbc793129854e4448e96608998caabe41
npaj <nicolas.pajusco@gmail.com>
2021-08-14 18:29:03 +0200

    add figures for dash app

--- analysis/Process_averaging_1min.py
+++ analysis/Process_averaging_1min.py
@@ -8,17 +8,21 @@ list_site = [4, 11, 25, 36, 38, 48, 52, 61, 62, 77, 87, 115, 120, 121, 132, 153,
 for site in list_site:
 
     Tagg = pd.read_csv(f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/tagging_site_{site:04d}.csv')
-    Tagg = Tagg.sort_values('name').reset_index(drop=True)
+    # Tagg = Tagg.sort_values('name').reset_index(drop=True)
+    indic = pd.read_csv(f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/site_{site:04d}.csv')
+    # indic = 
 
     file_save_name = f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/tagging_site_av_{site:04d}.csv'
 
     col = list(Tagg.columns)
-    col.pop(1)
+    col[1] = 'datetime'
+    for idx in range(2, len(col)-3):
+        col[idx] = col[idx][4:]
     seen = {}
 
     filemane = [seen.setdefault(x, x) for x in list(Tagg['name']) if x not in seen] 
 
-    out = pd.DataFrame(data = {'name':filemane}, columns=col)
+    out = pd.DataFrame(data = {'name': filemane}, columns=col)
 
     curr_name = ""
     idx = 0
@@ -30,8 +34,15 @@ for site in list_site:
             for lst_tag_idx in col :
                 if lst_tag_idx == 'name':
                     out[lst_tag_idx][idx] = filemane
+                elif lst_tag_idx == 'datetime':
+                    out[lst_tag_idx][idx] = indic[lst_tag_idx][idx_val].iloc[0]
+                
+                elif lst_tag_idx == 'geophony' or lst_tag_idx == 'biophony' or lst_tag_idx == 'anthropophony' :
+                    out[lst_tag_idx][idx] = np.mean(Tagg[f'{lst_tag_idx}'][idx_val])
+
                 else:
-                    out[lst_tag_idx][idx] = np.mean(Tagg[lst_tag_idx][idx_val])
+                    out[lst_tag_idx][idx] = np.mean(Tagg[f'tag_{lst_tag_idx}'][idx_val])
+
             idx += 1
             
         else:
--- visualisation/plotly_figs.py
+++ visualisation/plotly_figs.py
@@ -1,17 +1,15 @@
+import os
 import plotly.express as px
 import plotly.graph_objects as go
-
-# mapbox_access_token = open(".mapbox_token").read()#"pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
-
-
-# def get_map_fig(database):
-#     fig = px.scatter_mapbox(database, lat="lat", lon="lng", color = "s_statsmean",hover_data=['partID', 'city', 'country', "recorder"], zoom=3, height=300, width=500)
-#     fig.update_layout(
-#         mapbox_style="open-street-map",autosize=True,showlegend=False,
-#                 margin=go.layout.Margin(l=0, r=0, t=0, b=0))
-#     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-#     return fig
-
+from plotly.subplots import make_subplots
+import pandas as pd
+from datetime import datetime
+import librosa
+import numpy as np
+from scipy.signal import stft
+import sys
+sys.path.insert(0, "../")
+from utils import librosa_display as ld
 
 
 def get_map_fig(database):
@@ -27,3 +25,68 @@ def get_map_fig(database):
                         xanchor="right", x=1.0
                         ))
     return fig
+
+def get_heatmaps(site, path = '/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2'):
+
+    macro_cat = {'geophony':['Wind', 'Rain', 'River', 'Wave', 'Thunder'],
+                 'biophony': ['Bird', 'Amphibian', 'Insect', 'Mammal', 'Reptile'], 
+                 'anthropophony': ['Walking', 'Cycling', 'Beep', 'Car', 'Car honk', 'Motorbike', 'Plane', 'Helicoptere', 'Boat', 'Others_motors', 'Shoot', 'Bell', 'Talking', 'Music', 'Dog bark', 'Kitchen sounds', 'Rolling shutter']}
+    
+    data = pd.read_csv(os.path.join(path, f'tagging_site_av_{site:04d}.csv'))
+    for idx, k in enumerate(data['datetime']) :
+        data['datetime'][idx] = datetime.strptime(k, '%Y%m%d_%H%M%S')
+
+    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, subplot_titles=("<b>Geophony</b>", "<b>Biophony</b>", "<b>Anthropophony</b>", ""), vertical_spacing=0.03)
+
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['geophony'], z = data[macro_cat['geophony']].T, coloraxis='coloraxis', name='Geophony'), row=1, col=1)
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['biophony'], z = data[macro_cat['biophony']].T, coloraxis='coloraxis', name='Biophony'), row=2, col=1)
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['anthropophony'], z = data[macro_cat['anthropophony']].T, coloraxis='coloraxis', name='Anthropophony'), row=3, col=1)
+    fig.add_trace(go.Scatter(x = data['datetime'], y = data['geophony'], name="Geophony"), row=4, col=1)
+    fig.add_trace(go.Scatter(x = data['datetime'], y = data['biophony'], name= 'Biophony'), row=4, col=1)
+    fig.add_trace(go.Scatter(x = data['datetime'], y = data['anthropophony'], name='Anthropophony'), row=4, col=1)
+    fig.update_layout(coloraxis_colorbar=dict(
+        title="<b>Value</b>",titleside='right',
+                        thicknessmode="pixels", thickness=10,
+                        lenmode="pixels", len=350,
+                        yanchor="bottom", y=0.0,
+                        xanchor="right", x=1.1
+                        ))
+
+    return fig
+
+def get_sample_fig(site, file, path = '/Users/nicolas/Documents/SilentCities/SilentCities/fortesteco'):
+    path_audio = os.path.join(path, f'{site:04d}', file)
+    print(path_audio)
+    audio, sr = librosa.load(path_audio, sr = 44100)
+    N = len(audio)
+    Zxx = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=256, fmax=20000)
+    t, f, Zxx = ld.specshow(Zxx, sr = sr, fmax = 20000,y_axis='mel', x_axis='time', ref = np.max)
+
+    # f, t, Zxx = stft(audio, sr, nperseg=1024, noverlap=512, nfft=2048)
+    print(f)
+    print(Zxx.shape)
+    print('ok')
+
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("<b>Waveforme</b>", "<b>mel spactrogram</b>"), vertical_spacing=0.03)
+    fig.add_trace(go.Scattergl(x = np.arange(N)/sr, y = audio, name="Geophony"), row=1, col=1)
+    fig.add_trace(go.Heatmap(x = t, y = f, z = 10*np.log10(Zxx+1e-12), colorscale='thermal',zauto=False, zmin = -60, zmax=0), row=2, col=1)
+    fig.update_yaxes(title = 'Frequency (Hz)',type='log', range=[2,4],row=2, col=1)
+    fig.update_yaxes(title = 'Amplitude (-)',row=1, col=1)
+    fig.update_xaxes(title = 'Time (s)',row=2, col=1)
+    return fig
+    
+
+
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    # fig = get_heatmaps(25)
+    # fig.show()
+    fig = get_sample_fig(61, "5E81A700.WAV")
+    fig.show()
