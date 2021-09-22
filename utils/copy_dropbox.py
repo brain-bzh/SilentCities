2e3495aaf022b3b2d053cb96cb5aa66dcdca8b51
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2021-09-22 14:50:51 +0200

    version that copies the new folder

--- utils/copy_dropbox.py
+++ utils/copy_dropbox.py
@@ -1,5 +1,9 @@
 from zipfile import ZipFile
+import pandas as pd 
 import os
+from datetime import datetime
+now = datetime.now()
+date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
 # %% [markdown]
 # 1. For each folder or dropbox, search for an existing folder on bigdisk1 or bigdisk2. 
 # 2. If the folder is not found, this is a new site. 
@@ -15,9 +19,13 @@ bigdisk2sites = []
 
 for curdir in os.listdir(dropboxfolder):
     if os.path.isdir(os.path.join(bigdisk1,'silentcities',curdir)):
-        print(f"{curdir} found on bigdisk1")
+        nfiles = len(os.listdir(os.path.join(bigdisk1,'silentcities',curdir)))
+        print(f"{curdir} found on bigdisk1 with {nfiles} files or directories inside")
+        bigdisk1sites.append(curdir)    
     elif os.path.isdir(os.path.join(bigdisk2,'silentcities',curdir)):
-        print(f"{curdir} found on bigdisk2")
+        nfiles = len(os.listdir(os.path.join(bigdisk2,'silentcities',curdir)))
+        print(f"{curdir} found on bigdisk2 with {nfiles} files or directories inside")
+        bigdisk2sites.append(curdir)
     else:
         newsites.append(curdir)
         print(f"{curdir} is a new site")
@@ -39,12 +47,16 @@ def extract_list_from_zip(zipfile,destdir,filelist=None):
         filelist = z.namelist()
         print("Extracting all files...")
     
-    print(f"Extracting {len(filelist)}files")
+    print(f"Extracting {len(filelist)} files")
 
     for curfile in filelist:
+        extension = os.path.splitext(curfile)[1]
         
+        if not(extension.casefold() == '.wav'):
+            print("not a wav file")
+            continue
         # In case in the filelist there are paths
-        filenameonly = os.path.split(curfile)[1]
+        filenameonly = os.path.split(curfile)[1]        
 
         # extract a specific file from the zip container
         f = z.open(curfile)
@@ -55,4 +67,30 @@ def extract_list_from_zip(zipfile,destdir,filelist=None):
         f.write(content)
         f.close()
 
-print(newsites)
+Df=  pd.DataFrame(newsites)
+
+Df.to_csv(f"/bigdisk2/new/dropbox/newsites_{date_time}.csv",index=False,header=False)
+
+Df=  pd.DataFrame(bigdisk1sites)
+
+Df.to_csv(f"/bigdisk2/new/dropbox/bigdisk1_{date_time}.csv",index=False,header=False)
+
+Df=  pd.DataFrame(bigdisk2sites)
+
+Df.to_csv(f"/bigdisk2/new/dropbox/bigdisk2_{date_time}.csv",index=False,header=False)
+
+
+
+for cursite in newsites:
+    listzipfiles = (os.listdir(os.path.join(dropboxfolder,cursite)))
+    input(f'Press enter to continue to extract site {cursite}....')
+    for zipf in listzipfiles:
+        curzip = os.path.join(dropboxfolder,cursite,zipf)
+
+        curdestdir = os.path.join(bigdisk2,'silentcities',cursite)
+        os.makedirs(curdestdir,exist_ok=True)
+
+        print(f"Extracting {curzip} into {curdestdir}...")
+        
+        z = ZipFile(curzip)
+        z.extractall(curdestdir)
