2a9536a189ba194acde69fcaaddb1ec5c6aca0f7
npaj <nicolas.pajusco@gmail.com>
2021-10-10 18:21:37 +0200

    French version + manual classif

--- dash-app/app_FEIAT.py
+++ dash-app/app_FEIAT.py
@@ -4,12 +4,12 @@ sys.path.insert(0, "../")
 import os 
 from glob import glob
 # import argparse
-
+import csv
 import dash
 # import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 # from dash_html_components.Div import Div
 # from librosa.core import audio
 # import plotly.graph_objs as go
@@ -22,13 +22,13 @@ from datetime import datetime
 import base64
 
 ##### Path
-# PATH_MP3 = '/home/nfarrugi/bigdisk2/mp3_sl/'
-# PATH_DATABASE = "/home/nfarrugi/SilentCities/database/public_final_metadata_geo_stats.csv"
-# PATH_TAGSITE = "/home/nfarrugi/bigdisk2/meta_silentcities/site/"
+PATH_MP3 = '/home/nfarrugi/bigdisk2/mp3_sl/'
+PATH_DATABASE = "/home/nfarrugi/SilentCities/database/public_final_metadata_geo_stats.csv"
+PATH_TAGSITE = "/home/nfarrugi/bigdisk2/meta_silentcities/site/"
 
-PATH_MP3 = '/Users/nicolas/Downloads/mp3_sl/'
-PATH_DATABASE = "/Users/nicolas/Documents/SilentCities/database/public_final_metadata_geo_stats.csv"
-PATH_TAGSITE = "/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW3/"
+# PATH_MP3 = '/Users/nicolas/Downloads/mp3_sl/'
+# PATH_DATABASE = "/Users/nicolas/Documents/SilentCities/database/public_final_metadata_geo_stats.csv"
+# PATH_TAGSITE = "/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW3/"
 
 #### Initialization
 database = pd.read_csv(PATH_DATABASE)
@@ -45,6 +45,16 @@ figmap = pf.get_map_fig(database)
 figindic, data = pf.get_heatmaps(available_site[0], path=PATH_TAGSITE)
 wavefig, path_current_audio = pf.get_sample_fig(available_site[0], f"{data['name'][0][:-4]}_{int(data['start'][0])}.mp3", path=PATH_MP3)
 encoded_sound = base64.b64encode(open(path_current_audio, 'rb').read())
+
+# Init_csv_file
+header = ["site", 'file', 'datetime', 'current_time', 'Antropophy','Geophony', 'Biophony', 'comm']
+idx = 0
+LOGFILENAME = f'''logfile/{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv'''
+if not os.path.exists('logfile/'):
+    os.makedirs('logfile/')
+with open(LOGFILENAME, 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 
 
 # styles
@@ -77,7 +87,7 @@ app.layout = html.Div(
                 #     href="https://osf.io/h285u/",
                 #     style={"color": "white"},
                 # ),   
-                html.H2("Silent Cities Project : Data Analysis", style={"color": "white"}),
+                html.H2("Silent Cities Project : Visualisation des données", style={"color": "white"}),
                 # html.A(
                 #     id="gh-link",
                 #     children=["Source code Silent Cities"],
@@ -99,15 +109,15 @@ app.layout = html.Div(
                             children=[
                                 html.Div(className="row",children = [
                                 dcc.Graph(id='map', figure = figmap),
-                                html.H4(children=['Site informations :'],style={"color": "blue"}),
+                                html.H4(children=['Méta données:'],style={"color": "blue"}),
                                 html.H6(
                                     id="map-info",
                                     children=[f"Site: {database['partID'][0]}", html.Br(),
-                                            f"Country: {database['country'][0]}", html.Br(),
-                                            f"City : { database['city'][0]}",html.Br(),
-                                            f"Recorder : {database['recorder'][0]}",html.Br(),
-                                            f"Stat : {database['s_statsmean'][0]:.3f}", html.Br(),
-                                            f"Information : NA"],
+                                            f"Pays: {database['country'][0]}", html.Br(),
+                                            f"Ville : { database['city'][0]}",html.Br(),
+                                            f"Enregistreur : {database['recorder'][0]}",html.Br(),
+                                            f"Stat : {database['s_statsmean'][0]:.3f}"
+                                            ],
                                     
                                     style={"color": "black", "border": "solid 1px white"},
                                 ),    
@@ -126,13 +136,44 @@ app.layout = html.Div(
                     children=[
                     html.H6(
                                     id="wav-info",
-                                    children=[f"Wave file informations : file name {data['name'][0]}, date : {datetime.strptime(data['datetime'][0], '%Y%m%d_%H%M%S')}, Geophony : {data['geophony'][0]}, Biophony {data['biophony'][0]}, Anthropophony {data['anthropophony'][0]}"],
+                                    children=[f"Information fichier wav : file name {data['name'][0]}, date : {datetime.strptime(data['datetime'][0], '%Y%m%d_%H%M%S')}, Geophony : {data['geophony'][0]*100:.1f} %, Biophony {data['biophony'][0]*100:.1f} %, Anthropophony {data['anthropophony'][0]*100:.1f} %"],
                                     style={"color": "blue", "border": "solid 1px white"},
                                 ),  
                     html.Audio(id="player", src='data:audio/mpeg;base64,{}'.format(encoded_sound.decode()), controls=True, style={
                         "width": "100%"}),
                     dcc.Graph(id='spactrogram', figure = wavefig, style={
                         "width": "100%"})
+                    ]
+                ),
+                html.Div(className="row",
+                    children=[
+                        html.H3(id = 'text_what',
+                                children=['''Qu'est ce que vous entendez ?''']),
+                        html.Div(className="one-third column alpha",
+                                children=[
+                                    dcc.Checklist(
+                                        id="checklist",
+                                        options=[{'label':'Antropophonie', 'value':'Antropophony'},
+                                                {'label':'Géophonie', 'value':'Geophony'},
+                                                {'label':'Biophonie', 'value':'Biophony'},
+                                                {'label':'Bruit', 'value':'Bruit'}],
+                                        value=[],
+                                        # labelStyle={"display": "inline-block",'font_size': '38px', 'color':"red"},
+                                        # inputStyle={"display": "inline-block",'font_size': '26px'},
+                                    )]
+                            ),
+                        html.Div(className="row",
+                                children=[
+
+                                    dcc.Textarea(
+                                        id='text_comm',
+                                        value='',
+                                        style={'width': '50%', 'height': 100}
+                                    )
+                                    # html.Button('Enregistrer', id='textarea-state-example-button', n_clicks=0),
+                                ]
+                            )                
+                        
                     ]
                 )
             ]
@@ -157,12 +198,12 @@ def Update_heatmap(clickData):
     figindic, data = pf.get_heatmaps(current_partID, path=PATH_TAGSITE)
 
 
-    text = [f"Site: {database['partID'][idx]}", html.Br(),
-            f"Country: {database['country'][idx]}", html.Br(),
-            f"City : { database['city'][idx]}",html.Br(),
-            f"Recorder : {database['recorder'][idx]}",html.Br(),
+    text = [f"Site : {database['partID'][idx]}", html.Br(),
+            f"Pays : {database['country'][idx]}", html.Br(),
+            f"Ville : { database['city'][idx]}",html.Br(),
+            f"Enregistreur : {database['recorder'][idx]}",html.Br(),
             f"Stat : {database['s_statsmean'][idx]:.3f}", html.Br(),
-            f"Information : NA"]
+            ]
     print(text)
     return figindic, text
 
@@ -170,11 +211,23 @@ def Update_heatmap(clickData):
 
 @app.callback([Output('spactrogram', 'figure'),
                Output('player', 'src'),
-               Output('wav-info', 'children')],
-    [         Input('heatmap', 'clickData')])
-def Update_audio(clickData):
+               Output('wav-info', 'children'),
+               Output('text_comm', 'value'),
+               Output('checklist', 'value')],
+    [         Input('heatmap', 'clickData')],
+    [State('text_comm', 'value'),
+    State('checklist', 'value')])
+def Update_audio(clickData, val_text, val_check):
     global data
     global current_partID
+    global idx
+
+    print(val_text)
+    print(val_check)
+    print([current_partID, str(data['name'][idx])[:-4], data['datetime'][idx], datetime.now().strftime('%Y%m%d_%H%M%S'), 'Antropophony' in val_check, 'Geophony' in val_check, 'Biophony' in val_check, 'Bruit' in val_check, val_text])
+    with open(LOGFILENAME, 'a', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow([current_partID, str(data['name'][idx])[:-4], data['datetime'][idx], datetime.now().strftime('%Y%m%d_%H%M%S'), 'Antropophony' in val_check, 'Geophony' in val_check, 'Biophony' in val_check, 'Bruit' in val_check, val_text])
 
     x = clickData['points'][0]['x']
     idx = data.index[data["datetime"]==x][0]
@@ -184,12 +237,15 @@ def Update_audio(clickData):
     
     encoded_sound = base64.b64encode(open(path_current_audio, 'rb').read())
     src = 'data:audio/mpeg;base64,{}'.format(encoded_sound.decode())
-    text = [f"Wave file informations : file name {str(data['name'][idx])[:-4]}_{int(data['start'][idx])}.mp3, date : {datetime.strptime(data['datetime'][idx], '%Y%m%d_%H%M%S')}, Geophony : {data['geophony'][idx]}, Biophony {data['biophony'][idx]}, Anthropophony {data['anthropophony'][idx]}"]
+    text = [f"Information fichier wav : file name {str(data['name'][idx])[:-4]}_{int(data['start'][idx])}.mp3, date : {datetime.strptime(data['datetime'][idx], '%Y%m%d_%H%M%S')}",html.Br() ,f"Anthropophonie : {data['anthropophony'][idx]*100:.1f} %", html.Br() ,f"Geophonie : {data['geophony'][idx]*100:.1f} %, ", html.Br() ,f"Biophonie : {data['biophony'][idx]*100:.1f} %,"]
+    
+    return wavefig, src, text, '', []
 
-    return wavefig, src, text
+
+
 
 
     
 if __name__ == '__main__':
-    app.run_server(debug=True, host='127.0.0.1',port=os.getenv("PORT", "8051"))
+    app.run_server(debug=False, host='127.0.0.1',port=os.getenv("PORT", "8051"))
 
--- visualisation/plotly_figs.py
+++ visualisation/plotly_figs.py
@@ -40,22 +40,22 @@ def get_heatmaps(site, path):
         data = pd.read_csv(os.path.join(path, f'results_{site:04d}.csv'))
 
 
-    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, subplot_titles=("<b>Geophony</b>", "<b>Biophony</b>", "<b>Anthropophony</b>", ""), vertical_spacing=0.04)
+    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, subplot_titles=("<b>Anthropophonie</b>", "<b>Géophonie</b>", "<b>Biophonie</b>", ""), vertical_spacing=0.04)
 
-    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['geophony'], z = data[[f'tag_{idx}' for idx in macro_cat['geophony']]].T, coloraxis='coloraxis', name='Geophony'), row=1, col=1)
-    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['biophony'], z = data[[f'tag_{idx}' for idx in macro_cat['biophony']]].T, coloraxis='coloraxis', name='Biophony'), row=2, col=1)
-    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['anthropophony'], z = data[[f'tag_{idx}' for idx in macro_cat['anthropophony']]].T, coloraxis='coloraxis', name='Anthropophony'), row=3, col=1)
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['geophony'], z = data[[f'tag_{idx}' for idx in macro_cat['geophony']]].T, coloraxis='coloraxis', name='Géophonie',colorscale='Hot'), row=2, col=1)
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['biophony'], z = data[[f'tag_{idx}' for idx in macro_cat['biophony']]].T, coloraxis='coloraxis', name='Biophonie',colorscale='Hot'), row=3, col=1)
+    fig.add_trace(go.Heatmap(x = data['datetime'], y = macro_cat['anthropophony'], z = data[[f'tag_{idx}' for idx in macro_cat['anthropophony']]].T, coloraxis='coloraxis', name='Anthropophonie', colorscale='viridis'), row=1, col=1)
     # for idx in range(3):
     #     fig.add_trace(go.Scattergl(x = data['datetime'], y = data['tag_{}'.format(macro_cat['geophony'][idx])], name=macro_cat['geophony'][idx]), row=1, col=1)
     #     fig.add_trace(go.Scattergl(x = data['datetime'], y = data['tag_{}'.format(macro_cat['biophony'][idx])],  name=macro_cat['biophony'][idx]), row=2, col=1)
     #     fig.add_trace(go.Scattergl(x = data['datetime'], y = data['tag_{}'.format(macro_cat['anthropophony'][idx])],  name=macro_cat['anthropophony'][idx]), row=3, col=1)
-  
-    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['geophony'],name="Geophony"), row=4, col=1)
-    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['biophony'], name= 'Biophony'), row=4, col=1)
-    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['anthropophony'],name='Anthropophony'), row=4, col=1)
+    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['anthropophony'],name='Anthropophonie', line = dict(color='black'), opacity=0.5), row=4, col=1)
+    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['geophony'],name="Géophonie", line = dict(color='blue'), opacity=0.5), row=4, col=1)
+    fig.add_trace(go.Scattergl(x = data['datetime'], y = data['biophony'], name= 'Biophonie', line = dict(color='green'), opacity=0.5), row=4, col=1)
+    
    
     fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0},coloraxis_colorbar=dict(
-        title="<b>Value</b>",titleside='right',
+        title="<b>Probabilité</b>",titleside='right',
                         thicknessmode="pixels", thickness=10,
                         lenmode="pixels", len=150,
                         yanchor="bottom", y=0.0,
@@ -80,12 +80,12 @@ def get_sample_fig(site, file, path):
     # cmap = plt.get_cmap('jet')
     # rgba_img = cmap(10*np.log10(Zxx+1e-12))
     # rgb_img = np.delete(rgba_img, 3, 2)
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("<b>Waveforme</b>", "<b>Mel spectrogram</b>"), vertical_spacing=0.04)
-    fig.add_trace(go.Scattergl(x = (np.arange(N)/sr)[::100], y = audio[::100], name="waveforme"), row=1, col=1)
-    fig.add_trace(go.Heatmap(x = t, y = f, z = 10*np.log10(Zxx+1e-12), colorscale='thermal',zauto=False, zmin = -60, zmax=0, name='MelSpectrogram'), row=2, col=1)
-    fig.update_yaxes(title = 'Frequency (Hz)',type='log', range=[2,4],row=2, col=1)
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("<b>Représentation temporelle</b>", "<b>Spectrogramme</b>"), vertical_spacing=0.04)
+    fig.add_trace(go.Scattergl(x = (np.arange(N)/sr)[::100], y = audio[::100], name="Représentation temporelle"), row=1, col=1)
+    fig.add_trace(go.Heatmap(x = t, y = f, z = 10*np.log10(Zxx+1e-12), colorscale='thermal',zauto=False, zmin = -60, zmax=0, name='Spectrogramme'), row=2, col=1)
+    fig.update_yaxes(title = 'Fréquence (Hz)',type='log', range=[2,4],row=2, col=1)
     fig.update_yaxes(title = 'Amplitude (-)',row=1, col=1)
-    fig.update_xaxes(title = 'Time (s)',row=2, col=1)
+    fig.update_xaxes(title = 'Temps (s)',row=2, col=1)
     fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
     return fig, path_audio
     
