1d6f95b1bfc144a36c95886b2231943d5af9f8bd
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2022-10-09 18:09:10 +0200

    recoded for compatibility

--- analysis/Process_manual_validation.py
+++ analysis/Process_manual_validation.py
@@ -1,90 +1,76 @@
-import os
-import numpy as np
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from py import process
+import os 
+bigdiskpath = '/home/nfarrugi/bigdisk'
 
+bigdisk1path = bigdiskpath + '1'
+bigdisk2path = bigdiskpath + '2'
 
-manual_validation = pd.read_excel('/Users/nicolas/Documents/SilentCities/SilentCities/meta_silentcities/Table_Sound_Validation_sortedJF_20210408.xlsx')
-manual_validation = manual_validation.sort_values('Site').reset_index(drop=True)
+xlspath = '/home/nfarrugi/SilentCities/papers/SciDataPaper/analysis/identification_manuelle/Manual_validation_indic_corrected_blank.xlsx'
+xlspath_out = '/home/nfarrugi/SilentCities/papers/SciDataPaper/analysis/identification_manuelle/Manual_validation_indic_corrected_final_max.xlsx'
 
-list_site = [4, 11, 25, 36, 38, 48, 52, 61, 62, 77, 87, 115, 120, 121, 132, 153, 158, 159, 190, 229, 234, 276, 292, 346, 371, 388]
+Df = pd.read_excel(xlspath,index_col=0)
 
+## Detect lines with missing values and extract the corresponding Dataframe
+Df_missing = Df.loc[pd.isna(Df['tag_Bell'])]
 
-indic = {'dB': [], 'ndsi_N': [], 'aci_N': [],
-                        'BI_N': [], 'EAS_N': [],
-                        'ECV_N': [], 'EPS_N': [] ,'ndsi_W': [], 'aci_W': [],
-                        'BI_W': [], 'EAS_W': [],
-                        'ECV_W': [], 'EPS_W': [], 'ACT':[],
-                    'POWERB_126':[], 'POWERB_251':[], 'POWERB_501':[], 'POWERB_1k':[], 'POWERB_2k':[], 'POWERB_4k':[], 'POWERB_8k':[], 'POWERB_16k':[]}
+missingsites = Df_missing['Site'].unique()
 
+### match all the results output and fill the missing columns
 
+for site in missingsites:
+    cursite = "%04d" % (site)
+    print(f"Site {cursite}")
 
-lst_indic = indic.keys()
-
-
-for cur_idx in lst_indic:
-    manual_validation[cur_idx] = None
-
-add_col = True
-for site in list_site:
-
-    Df = pd.read_csv(f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW3/site_{site:04d}.csv')
-    Tagg = pd.read_csv(f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/tagging_site_{site:04d}.csv')
-    if add_col:
-        lst_tag = Tagg.columns[2:]
-        for lst_tag_idx in lst_tag : manual_validation[lst_tag_idx] = None 
-        add_col = False
-
-    Df['name'] = [oo.casefold() for oo in Df['name'] ]
-
+    allfiles = Df_missing.loc[Df_missing['Site']==site,'Name_recording']
     
-    for idx, name in enumerate(manual_validation['Name_recording']):
+    # Load the processed file for this site
+    processedDf = pd.read_csv(os.path.join(bigdisk2path,'meta_silentcities','site',f"results_{cursite}.csv"))
+    processedDf['name2'] = [i.casefold() for i in processedDf['name']]
+    for curfile in allfiles:
+        if curfile.casefold()[-4:] != '.wav':
+            curfile_ext = curfile.casefold()+'.wav'
+        else:
+            curfile_ext = curfile
+        subDf = processedDf.loc[processedDf['name2']==curfile_ext]
+        if len(subDf)==0:
+            subDf = processedDf.loc[processedDf['name']==curfile_ext]
+        
+        if len(subDf)>0:
+            lst_tag = subDf.columns[3:-1]
+            #print(f"Site {cursite} Found file {curfile} setting values..")
+            for lst_tag_idx in lst_tag:
+                if (lst_tag_idx[:3]=='tag') or (lst_tag_idx=='biophony') or (lst_tag_idx=='geophony') or (lst_tag_idx=='anthropophony'):
+                    Df_missing.loc[((Df_missing['Site']==site) & (Df_missing['Name_recording']==curfile)),lst_tag_idx] = np.max(subDf[lst_tag_idx].to_numpy())
+                else:
+                    Df_missing.loc[((Df_missing['Site']==site) & (Df_missing['Name_recording']==curfile)),lst_tag_idx] = np.mean(subDf[lst_tag_idx].to_numpy())
 
-        try:
-            if name.casefold()[-4:] != '.wav':
-                name = name+'.wav'
-            idx_val = list(Df['name'] == str(name).casefold() )
-
-            if idx_val.count(True) == 0:
-                pass
-            else:
-                for cur_idx in lst_indic:
-                    manual_validation[cur_idx][idx] = np.mean(Df[cur_idx][idx_val])
-                
-                
-                
-                for lst_tag_idx in lst_tag :
-                    manual_validation[lst_tag_idx][idx] = np.mean(Tagg[lst_tag_idx][idx_val])
-
-        except ValueError:
-
-            pass
-
-print(manual_validation)        
-manual_validation.to_excel('Manual_validation_indic.xlsx')
-manual_validation.to_csv('Manual_validation_indic.csv')
-
-
-# for site in list_site:
-
-#     Tagg = pd.read_csv(f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/tagging_site_{site:04d}.csv')
-#     Tagg = Tagg.sort_values('name').reset_index(drop=True)
-    
-#     list_name = []
-#     curr_name = 0
-#     for idx, filemane in enumerate(Tagg['name']):
-#         if curr_name != filemane:
-#             list_site.append(filemane)
-
-#         curr_name = filemane
-
-#     file_save_name = f'/Users/nicolas/Documents/SilentCities/SilentCities/ecoacoustique/NEW2/tagging_site_av_{site:04d}.csv'
-
-#     col = Tagg.columns
-#     out = pd.DataFrame(columns=col)
-
-#     for idx, file in enumerate(list_site):
-#         for keys in col:
+                        
+        
+Df_part = Df.dropna(axis=0,how='any')
+Df_final = pd.concat([Df_part,Df_missing]).sort_values(by=['Site'])
+Df_final.to_excel(xlspath_out)
+        
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Df_missing[['Site','Name_recording']].to_csv('missingfiles.csv',index=False)
+
+#print(Df_missing['Site'].unique())
 
