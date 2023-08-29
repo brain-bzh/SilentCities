ebbdd6a98c90683c87e489c6b506376c2cf4586d
nicofarr <nicofarr09@gmail.com>
2023-08-29 12:59:41 +0200

    prepmach

--- preparemachine.sh
+++ preparemachine.sh
@@ -1,7 +1,8 @@
-parallel-ssh -A -i -h /home/nfarrugi/hosts.txt tmux new -d -s nfa-homedir-persistent 'krenew -K 60 watch ls'
-parallel-ssh -A -i -h /home/nfarrugi/hosts.txt "mkdir -p /users/local/bigdisk1 ; mkdir -p /users/local/bigdisk2 ; sshfs sl-tp-br-008.imta.fr:/bigdisk1 /users/local/bigdisk1 ; sshfs sl-tp-br-008.imta.fr:/bigdisk2 /users/local/bigdisk2"
+parallel-ssh -A -i -h hosts tmux new -d -s nfa-homedir-persistent 'krenew -K 60 watch ls'
+parallel-ssh -A -i -h hosts "mkdir -p /users/local/bigdisk1 ; mkdir -p /users/local/bigdisk2 ; sshfs sl-tp-br-513.imta.fr:/bigdisk1 /users/local/bigdisk1 ; sshfs sl-tp-br-513.imta.fr:/bigdisk2 /users/local/bigdisk2"
 #squeue -o %.18i %.9P %.20j %.8u %.2t %.10M %.6D %R
-
+#TEST COMMAND
+#python audio_processing.py --metadata_folder /users3/local/bigdisk2/meta_silentcities/TEST/ --site 0000 --folder /users3/local/bigdisk1/silentcities/0000/ --database /users3/local/bigdisk2/meta_silentcities/TEST/database_pross.pkl --batch_size 64
 
 
 
