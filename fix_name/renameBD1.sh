fa646637e57ce38a05e18a70fe55e6f1d5c63cfc
nicofarr <nicofarr09@gmail.com>
2023-12-06 11:37:04 +0100

    cleaning

--- fix_name/renameBD1.sh
+++
@@ -1,3 +0,0 @@
-python 0128.py --folder /bigdisk1/silentcities/0128
-python 0149.py --folder /bigdisk1/silentcities/0149
-python 0176.py --folder /bigdisk1/silentcities/0176
--- fix_name/renameBD2.sh
+++
@@ -1,2 +0,0 @@
-python 0386.py --folder /bigdisk2/silentcities/0386
-python 0050.py --folder /bigdisk2/silentcities/0050
